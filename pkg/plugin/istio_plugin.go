b0e5a083ad2d082cf4bffb8463ec2c1b82dcfa76
Ralf Pannemans <ralf.pannemans@sap.com>
2018-11-28 14:53:47 +0100

    Call adapt_credentials on provider side.
    Co-authored-by: Ulrich Kramer <u.kramer@sap.com>

--- pkg/plugin/istio_plugin.go
+++ pkg/plugin/istio_plugin.go
@@ -2,11 +2,14 @@ package plugin
 
 import (
 	"encoding/json"
-	"github.com/Peripli/service-manager/pkg/web"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
 	"github.com/Peripli/istio-broker-proxy/pkg/model"
 	"github.com/Peripli/istio-broker-proxy/pkg/router"
-	"log"
-	"strings"
+	"github.com/Peripli/service-manager/pkg/web"
 )
 
 type IstioPlugin struct {
@@ -17,33 +20,98 @@ func (i *IstioPlugin) Name() string {
 	return "istio"
 }
 
+func logError(err error) {
+	log.Printf("error occured: %s\n", err.Error())
+}
+
 func (i *IstioPlugin) Bind(request *web.Request, next web.Handler) (*web.Response, error) {
 	var bindRequest model.BindRequest
-	log.Printf("IstioPlugin bind was triggered with request body: %s\n", string(request.Body))
-	json.Unmarshal(request.Body, &bindRequest)
+	log.Printf("IstioPlugin bind was triggered with request adaptRequestBody: %s\n", string(request.Body))
+	err := json.Unmarshal(request.Body, &bindRequest)
+	if err != nil {
+		logError(err)
+		return &web.Response{StatusCode: http.StatusBadRequest}, nil
+	}
 	log.Println("execute prebind")
 	bindRequest = *i.interceptor.PreBind(bindRequest)
 	request.Body, _ = json.Marshal(bindRequest)
-	response, _ := next.Handle(request)
-	var bindResponse model.BindResponse
-	json.Unmarshal(response.Body, &bindResponse)
-	log.Println("execute postbind")
-	modifiedBindResponse, err := i.interceptor.PostBind(bindRequest, bindResponse, extractBindId(request.URL.Path), model.Adapt)
+
+	response, err := next.Handle(request)
 	if err != nil {
-		log.Printf("Error during PostBind %s\n", err.Error())
+		logError(err)
+		return nil, err
 	}
-	response.Body, _ = json.Marshal(modifiedBindResponse)
+
+	if response.StatusCode/100 != 2 {
+		logError(err)
+		return response, nil
+	}
+	var bindResponse model.BindResponse
+	err = json.Unmarshal(response.Body, &bindResponse)
+	if err != nil {
+		logError(err)
+		return nil, err
+	}
+	log.Println("execute postbind")
+	modifiedBindResponse, err := i.interceptor.PostBind(bindRequest, bindResponse, extractBindId(request.URL.Path),
+		func(credentials model.Credentials, mappings []model.EndpointMapping) (*model.BindResponse, error) {
+			return i.AdaptCredentials(credentials, mappings, next, request)
+		})
+	if err != nil {
+		logError(err)
+		httpError, ok := err.(model.HttpError)
+		if ok {
+			return &web.Response{StatusCode: httpError.Status}, nil
+		}
+		return nil, err
+	}
+	response.Body, err = json.Marshal(modifiedBindResponse)
+	if err != nil {
+		logError(err)
+		return nil, err
+	}
 	return response, nil
 }
 
+func (i *IstioPlugin) AdaptCredentials(credentials model.Credentials, endpointMappings []model.EndpointMapping, next web.Handler, bindRequest *web.Request) (*model.BindResponse, error) {
+	request := web.Request{Request: bindRequest.Request, PathParams: bindRequest.PathParams}
+	request.URL.Path = request.URL.Path + "/adapt_credentials"
+	request.Method = http.MethodPost
+	adaptCredentialsRequest := model.AdaptCredentialsRequest{credentials, endpointMappings}
+	var err error
+	request.Body, err = json.Marshal(&adaptCredentialsRequest)
+	if err != nil {
+		logError(err)
+		return nil, err
+	}
+	response, err := next.Handle(&request)
+	if err != nil {
+		logError(err)
+		return nil, err
+	}
+	if response.StatusCode/100 != 2 {
+		httpError := model.HttpError{Status: response.StatusCode, Message: fmt.Sprintf("Error during call of adapt credentials")}
+		logError(httpError)
+		return nil, httpError
+	}
+	var bindResponse model.BindResponse
+	err = json.Unmarshal(response.Body, &bindResponse)
+	if err != nil {
+		logError(err)
+		return nil, err
+	}
+	return &bindResponse, nil
+}
+
 func (i *IstioPlugin) Unbind(request *web.Request, next web.Handler) (*web.Response, error) {
-	log.Printf("IstioPlugin unbind was triggered with request body: %s\n", string(request.Body))
+	log.Printf("IstioPlugin unbind was triggered with request adaptRequestBody: %s\n", string(request.Body))
 	i.interceptor.PostDelete(extractBindId(request.URL.Path))
 	return next.Handle(request)
 }
 
 func extractBindId(path string) string {
-	return strings.Split(path, "/")[3]
+	slices := strings.Split(path, "/")
+	return slices[len(slices)-1]
 }
 
 func createConsumerInterceptor() router.ConsumerInterceptor {
---
+++ pkg/plugin/istio_plugin_test.go
@@ -0,0 +1,297 @@
+package plugin
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/Peripli/istio-broker-proxy/pkg/router"
+
+	"github.com/pkg/errors"
+
+	"github.com/Peripli/istio-broker-proxy/pkg/model"
+	"github.com/Peripli/service-manager/pkg/web"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestIstioPluginBind(t *testing.T) {
+	g := NewGomegaWithT(t)
+	var err error
+	interceptor := SpyInterceptor{}
+	plugin := IstioPlugin{interceptor: &interceptor}
+	nextHandler := SpyWebHandler{}
+	sourceEndpoint := model.Endpoint{Host: "host2", Port: 8888}
+	targetEndpoint := model.Endpoint{Host: "host2", Port: 8888}
+	nextHandler.bindResponseBody, _ = json.Marshal(model.BindResponse{Endpoints: []model.Endpoint{sourceEndpoint}})
+	nextHandler.adaptResponseBody, _ = json.Marshal(model.BindResponse{Endpoints: []model.Endpoint{targetEndpoint}})
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	expectedBindRequest := model.BindRequest{
+		NetworkData:          model.NetworkDataRequest{NetworkProfileId: "test"},
+		AdditionalProperties: map[string]json.RawMessage{}}
+	request := web.Request{Request: &origRequest}
+	request.Body, err = json.Marshal(expectedBindRequest)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	response, err := plugin.Bind(&request, &nextHandler)
+
+	g.Expect(err).NotTo(HaveOccurred())
+
+	var bindRequest model.BindRequest
+	err = json.Unmarshal(nextHandler.bindRequestBody, &bindRequest)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(bindRequest).To(Equal(expectedBindRequest))
+
+	var bindResponse model.BindResponse
+	err = json.Unmarshal(response.Body, &bindResponse)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	g.Expect(interceptor.bindId).To(Equal("34234234234-43535-345345345"))
+
+}
+
+func TestIstioPluginBindForbidden(t *testing.T) {
+	g := NewGomegaWithT(t)
+	var err error
+	plugin := IstioPlugin{interceptor: router.NoOpInterceptor{}}
+	nextHandler := SpyWebHandler{bindStatusCode: http.StatusForbidden}
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	expectedBindRequest := model.BindRequest{
+		NetworkData:          model.NetworkDataRequest{NetworkProfileId: "test"},
+		AdditionalProperties: map[string]json.RawMessage{}}
+	request := web.Request{Request: &origRequest}
+	request.Body, err = json.Marshal(expectedBindRequest)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	response, err := plugin.Bind(&request, &nextHandler)
+
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(response.StatusCode).To(Equal(http.StatusForbidden))
+
+}
+
+func TestIstioPluginBindInvalidInput(t *testing.T) {
+	g := NewGomegaWithT(t)
+	var err error
+	plugin := IstioPlugin{interceptor: router.NoOpInterceptor{}}
+	nextHandler := SpyWebHandler{}
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	request := web.Request{Request: &origRequest, Body: []byte("sdfsf")}
+	g.Expect(err).NotTo(HaveOccurred())
+
+	response, err := plugin.Bind(&request, &nextHandler)
+
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(response.StatusCode).To(Equal(http.StatusBadRequest))
+
+}
+
+func TestIstioPluginBindHandleFails(t *testing.T) {
+	g := NewGomegaWithT(t)
+	var err error
+	plugin := IstioPlugin{interceptor: router.NoOpInterceptor{}}
+	nextHandler := SpyWebHandler{err: errors.New("oops")}
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	request := web.Request{Request: &origRequest, Body: []byte("{}")}
+	g.Expect(err).NotTo(HaveOccurred())
+
+	_, err = plugin.Bind(&request, &nextHandler)
+
+	g.Expect(err).To(HaveOccurred())
+
+}
+
+func TestIstioPluginBindInvalidBindResponse(t *testing.T) {
+	g := NewGomegaWithT(t)
+	var err error
+	plugin := IstioPlugin{interceptor: router.NoOpInterceptor{}}
+	nextHandler := SpyWebHandler{}
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	request := web.Request{Request: &origRequest, Body: []byte("{}")}
+	g.Expect(err).NotTo(HaveOccurred())
+
+	_, err = plugin.Bind(&request, &nextHandler)
+
+	g.Expect(err).To(HaveOccurred())
+
+}
+
+func TestIstioPluginBindOkButAdaptForbidden(t *testing.T) {
+	g := NewGomegaWithT(t)
+	var err error
+	plugin := IstioPlugin{interceptor: router.ConsumerInterceptor{}}
+	nextHandler := SpyWebHandler{adaptStatusCode: http.StatusForbidden, bindResponseBody: []byte("{}")}
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	request := web.Request{Request: &origRequest, Body: []byte("{}")}
+	g.Expect(err).NotTo(HaveOccurred())
+
+	response, err := plugin.Bind(&request, &nextHandler)
+
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(response.StatusCode).To(Equal(http.StatusForbidden))
+
+}
+
+func TestIstioPluginBindInvalidAdaptCredentialsResponse(t *testing.T) {
+	g := NewGomegaWithT(t)
+	var err error
+	plugin := IstioPlugin{interceptor: router.ConsumerInterceptor{}}
+	nextHandler := SpyWebHandler{bindResponseBody: []byte("{}")}
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	request := web.Request{Request: &origRequest, Body: []byte("{}")}
+	g.Expect(err).NotTo(HaveOccurred())
+
+	_, err = plugin.Bind(&request, &nextHandler)
+
+	g.Expect(err).To(HaveOccurred())
+
+}
+
+func TestIstioPluginAdaptCredentials(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	plugin := IstioPlugin{interceptor: router.NoOpInterceptor{}}
+	nextHandler := SpyWebHandler{}
+
+	targetEndpoint := model.Endpoint{Host: "host2", Port: 8888}
+	nextHandler.adaptResponseBody, _ = json.Marshal(model.BindResponse{Endpoints: []model.Endpoint{targetEndpoint}})
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	var origHeader http.Header = map[string][]string{"Authentication": {"Bearer"}}
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut, Header: origHeader}
+	bindRequest := web.Request{Request: &origRequest}
+
+	credentials := model.Credentials{AdditionalProperties: map[string]json.RawMessage{"password": json.RawMessage([]byte(`"abc"`))}}
+	endpointMappings := []model.EndpointMapping{{Source: model.Endpoint{Host: "host", Port: 1234}, Target: targetEndpoint}}
+	adaptResponse, err := plugin.AdaptCredentials(credentials, endpointMappings, &nextHandler, &bindRequest)
+
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(nextHandler.url.Path).To(Equal("/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345/adapt_credentials"))
+	g.Expect(nextHandler.method).To(Equal(http.MethodPost))
+	g.Expect(nextHandler.requestHeaders).To(Equal(origHeader))
+
+	var adaptRequest model.AdaptCredentialsRequest
+
+	err = json.Unmarshal(nextHandler.adaptRequestBody, &adaptRequest)
+
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(adaptRequest.Credentials).To(Equal(credentials))
+	g.Expect(adaptRequest.EndpointMappings).To(Equal(endpointMappings))
+
+	g.Expect(adaptResponse.Endpoints).To(Equal([]model.Endpoint{targetEndpoint}))
+
+}
+
+func TestIstioPluginAdaptCredentialsError(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	plugin := IstioPlugin{}
+	nextHandler := SpyWebHandler{err: errors.New("oops")}
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	bindRequest := web.Request{Request: &origRequest}
+
+	targetEndpoint := model.Endpoint{Host: "host2", Port: 8888}
+	credentials := model.Credentials{AdditionalProperties: map[string]json.RawMessage{"password": json.RawMessage([]byte(`"abc"`))}}
+	endpointMappings := []model.EndpointMapping{{Source: model.Endpoint{Host: "host", Port: 1234}, Target: targetEndpoint}}
+	_, err := plugin.AdaptCredentials(credentials, endpointMappings, &nextHandler, &bindRequest)
+
+	g.Expect(err).To(HaveOccurred())
+}
+
+func TestIstioPluginAdaptCredentialsInvalidResponse(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	plugin := IstioPlugin{}
+	nextHandler := SpyWebHandler{adaptResponseBody: []byte("dfsf")}
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	bindRequest := web.Request{Request: &origRequest}
+
+	targetEndpoint := model.Endpoint{Host: "host2", Port: 8888}
+	credentials := model.Credentials{AdditionalProperties: map[string]json.RawMessage{"password": json.RawMessage([]byte(`"abc"`))}}
+	endpointMappings := []model.EndpointMapping{{Source: model.Endpoint{Host: "host", Port: 1234}, Target: targetEndpoint}}
+	_, err := plugin.AdaptCredentials(credentials, endpointMappings, &nextHandler, &bindRequest)
+
+	g.Expect(err).To(HaveOccurred())
+}
+
+func TestIstioPluginAdaptCredentialsBadRequest(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	plugin := IstioPlugin{}
+	nextHandler := SpyWebHandler{adaptResponseBody: []byte(""), adaptStatusCode: http.StatusBadRequest}
+
+	origURL, _ := url.Parse("http://host:80/v2/service_instances/3234234-234234-234234/service_bindings/34234234234-43535-345345345")
+	origRequest := http.Request{URL: origURL, Method: http.MethodPut}
+	bindRequest := web.Request{Request: &origRequest}
+
+	targetEndpoint := model.Endpoint{Host: "host2", Port: 8888}
+	credentials := model.Credentials{AdditionalProperties: map[string]json.RawMessage{"password": json.RawMessage([]byte(`"abc"`))}}
+	endpointMappings := []model.EndpointMapping{{Source: model.Endpoint{Host: "host", Port: 1234}, Target: targetEndpoint}}
+	_, err := plugin.AdaptCredentials(credentials, endpointMappings, &nextHandler, &bindRequest)
+
+	g.Expect(err).To(HaveOccurred())
+	g.Expect(err.Error()).To(Equal("Error during call of adapt credentials"))
+	g.Expect(err.(model.HttpError).Status).To(Equal(http.StatusBadRequest))
+}
+
+type SpyWebHandler struct {
+	url               url.URL
+	method            string
+	adaptRequestBody  []byte
+	adaptResponseBody []byte
+	adaptStatusCode   int
+	bindRequestBody   []byte
+	bindResponseBody  []byte
+	bindStatusCode    int
+	requestHeaders    http.Header
+	err               error
+}
+
+func (s *SpyWebHandler) Handle(req *web.Request) (resp *web.Response, err error) {
+	s.url = *req.Request.URL
+	s.method = req.Request.Method
+	s.requestHeaders = req.Header
+	if strings.HasSuffix(s.url.Path, "adapt_credentials") {
+		if s.adaptStatusCode == 0 {
+			s.adaptStatusCode = http.StatusOK
+		}
+		s.adaptRequestBody = req.Body
+		return &web.Response{Body: s.adaptResponseBody, StatusCode: s.adaptStatusCode}, s.err
+	} else {
+		if s.bindStatusCode == 0 {
+			s.bindStatusCode = http.StatusOK
+		}
+		s.bindRequestBody = req.Body
+		return &web.Response{Body: s.bindResponseBody, StatusCode: s.bindStatusCode}, s.err
+	}
+}
+
+type SpyInterceptor struct {
+	router.NoOpInterceptor
+	bindId string
+}
+
+func (s *SpyInterceptor) PostBind(request model.BindRequest, response model.BindResponse, bindingId string,
+	adapt func(model.Credentials, []model.EndpointMapping) (*model.BindResponse, error)) (*model.BindResponse, error) {
+	s.bindId = bindingId
+	return &response, nil
+}
